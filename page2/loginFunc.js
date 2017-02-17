608c04575329707c5631491fb0f6e15b90c5f765
BATTLESURFACE <hailhailHaily@hotmail.com>
2017-02-17 13:41:58 +0800

    移動FB內容到Critical 資料夾，不包含在repository裡。測試性的作法，主要避免某些資料曝光。

--- page2/loginFunc.js
+++ page2/loginFunc.js
@@ -1,26 +1,5 @@
 
-//fbAPI init 開始
 
-   window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1423593324331860',
-      xfbml      : true,
-      version    : 'v2.8'
-    });
-    FB.AppEvents.logPageView();
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "http://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-   
-   
-//fbAPI init 結束
 
 
 
@@ -49,7 +28,7 @@
  
  //fb使用的方法
  function getFbLogin(){
-		
+		console.log(FB);
  }
  
  
