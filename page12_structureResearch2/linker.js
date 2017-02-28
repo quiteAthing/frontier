a3766ca63a522b666fb62f6c2bbd57bc37049ce2
BATTLESURFACE <hailhailHaily@hotmail.com>
2017-02-28 18:16:12 +0800

    繼續完整結構

--- page12_structureResearch2/linker.js
+++ page12_structureResearch2/linker.js
@@ -1,11 +1,26 @@
 
-const agree_to_term="";
-const btn_login="#login";
-const btn_logout="#logOut";
-const btn_continueFB="#facebook_login";
-const field_ACC="#acc_field";
-const fieldPW="#pwd_field";
-const fieldCPW="";//確認密碼
+
+//登入畫面
+const agree_to_term="accept-terms";//取得
+const btn_login="#login-submit";//一般登入
+const btn_logout="#logOut";//登出(不區分))
+const btn_continueFB="#login-fb";//FB登入
+const btn_register="#reg-submit";//註冊按鈕
+const field_ACC="#signup-email";//帳號欄
+const fieldPW="#signup-password";//密碼欄
+const fieldCPW="#signup-password-confirm";//確認密碼
+const fieldRemember_me="remember-me";//記住我
+
+
+//會員資料編輯畫面
+const btn_uData_submit="#edit-submit";
+const field_pic="#member-pic";
+const field_instrument="#member-instruments";//數列：X,2,3,4,5;
+const field_nickname="#member-name";
+const field_uIntro="#member-edit-textarea";
+const field_links="#member-media";//數列：X,2,3,4,5,6;
+const btn_send_message="#send-message"//發送站內信
+
 
 //網站狀態
 var sta=0;
--- page12_structureResearch2/setup.js
+++ page12_structureResearch2/setup.js
@@ -3,8 +3,14 @@
 
 $(document).ready(changeState);
 
-
+//這樣還沒有改變嗎?
 function changeState(){
+	//檢查是否登入過且未登出且cookie/session仍有效，若有，則顯示已登入畫面。
+	if(checkLoggedIn())
+	{
+		sta=1;
+		fetchUserData();
+		}
 	initElement();
 }
 function loginNormal(){
@@ -18,8 +24,14 @@ function loginNormal(){
 }
 
 function loginFb(){
-	console.log(getFbInfo());
 	getLoginInfo(getFbInfo(),onLogIn);
+}
+
+//載入頁面時檢查是否有登入過且未登出之cookie，檢查完後return true或false
+function checkLoggedIn(){
+	//檢查cookie是否存在，若存在，向伺服器詢問，若伺服器回傳正常，回傳true
+	//若不存在，回傳false。
+	return true;
 }
 
 function initElement(){
@@ -63,25 +75,36 @@ function onLogIn(success){
 }
 	//登入失敗的處理
 function onLogFailed(){
-	
+	showLogFailed();
 }
 
 
 function onLogOut(){
 	initElement();
-	eraseData();
+	reload();
+	//登出後重新載入葉面
 }
 
 
 function fetchUserData(){
-	//這邊理論上是要去跟伺服器要資料啦，不過這邊用假資料解決
+	//這邊理論上是要去跟伺服器要資料啦，不過這邊現在用cookie裡的假資料解決
 	var data=extractCookie(jam_cookie_key);
 	showData(data);
 }
-
-//實際改變顯示內容的方法
+/*
+//實際改變顯示內容的方法，交給設計師操作
 function showData(uInfo){
 	$("#statusMonitor").text("loginCookie"+uInfo);
+}
+
+//實際改變顯示內容的方法2
+function showLogFailed(){
+	
+}
+*/
+
+function reload(){
+	
 }
 
 function eraseData(){
