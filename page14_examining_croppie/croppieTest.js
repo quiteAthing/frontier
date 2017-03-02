93b1eec41b46124be864ca0477e2b0576cd1d87f
BATTLESURFACE <hailhailHaily@hotmail.com>
2017-03-02 16:12:26 +0800

    no message

--- page14_examining_croppie/croppieTest.js
+++ page14_examining_croppie/croppieTest.js
@@ -1,11 +1,29 @@
-/**
- * 
- */
 
-
-
- 
- 
 
 
 	
+window.addEventListener("load",setup);
+
+
+var c=undefined;
+
+function setup(){
+	var opts=new Object();
+	opts.viewport={width : 144,height : 144 , type : "circle"};
+	opts.showZoomer=false;
+	c=new Croppie($("#cropArea")[0],opts);
+	$("#setIMG").on("change",onImgSet);
+}
+
+
+function onImgSet(event){
+	var fr=new FileReader();
+	fr.readAsDataURL(event.target.files[0]);
+	fr.onload=function (e){
+		var stuff=new Object();
+		stuff.url=e.target.result;
+		stuff.points=[0,0,0,0];
+		stuff.orientation=1;
+		c.bind(stuff);
+	}
+}
