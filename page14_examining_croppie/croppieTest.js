dd526c0811120a3c8a176ad11ff22387ff626d2a
PC-26\PC-26 <aaaa>
2017-03-03 11:51:45 +0800

    Croppie可動。

--- page14_examining_croppie/croppieTest.js
+++ page14_examining_croppie/croppieTest.js
@@ -13,6 +13,7 @@ function setup(){
 	opts.showZoomer=false;
 	c=new Croppie($("#cropArea")[0],opts);
 	$("#setIMG").on("change",onImgSet);
+	$("#getIMG").on("click",onImgCropped);
 }
 
 
@@ -22,8 +23,39 @@ function onImgSet(event){
 	fr.onload=function (e){
 		var stuff=new Object();
 		stuff.url=e.target.result;
-		stuff.points=[0,0,0,0];
+		stuff.points=[200,200,400,400];
 		stuff.orientation=1;
 		c.bind(stuff);
 	}
 }
+
+function onImgCropped(){
+	var rAttr=new Object();
+	rAttr.type="base64";
+	rAttr.format="jpeg";	
+	rAttr.size="viewport";	
+	rAttr.quality=1;	
+	rAttr.circle=true;	
+	
+	c.result(rAttr).then(function(rs){
+		var img=new Image();
+		img.src=rs;
+		img.onload=putIn;
+	});
+}
+
+
+
+ function putIn(){
+	 console.log(this);
+	var cns=document.getElementById("rst");
+	var ctx=cns.getContext("2d");
+	ctx.clearRect(0,0,cns.width,cns.height);
+	ctx.beginPath();
+	ctx.arc(72,72,cns.width/2.25,Math.PI*2,0, false);
+	ctx.clip("evenodd");
+	ctx.drawImage(this,0,0,144,144);
+
+ }
+ 
+ 
