d883f3d3d50c358331a836ff4ab84e8ec4783106
Kslander <hailhailHaily@hotmail.com>
2017-03-04 13:13:02 +0800

    東西準備得差不多，可以準備整合到網頁上了。

--- lib_object_form/backgroundTasks.js
+++ lib_object_form/backgroundTasks.js
@@ -3,6 +3,12 @@
 
 var bgts=new Object();
 
+bgt.taskList=new Array();
+
 bgts.bgtLoggedOut=function(){}
 
 bgts.bgtLoggedIn=function(){}
+
+bgts.activateBgt=function(interval,job){
+	this.taskList.push(setInterval(job,interval));
+}
